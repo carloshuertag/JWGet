fb06ef4139e7eef8cf2f53bfd251326fb76b41c9
Carlos Huerta Garcia <huerta2502@gmail.com>
2021-11-26 15:54:53 +0000

    Fixed http request. content type and content length are well known by from http response

--- src/JWGet/JWGet.java
+++ src/JWGet/JWGet.java
@@ -1,6 +1,10 @@
 package JWGet;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -21,26 +25,69 @@ public class JWGet {
         StringTokenizer tokenizer = new StringTokenizer(arg, "/");
         tokenizer.nextToken();
         String host = tokenizer.nextToken();
-        InetAddress ipAddress;
         int p = host.indexOf(':'), port = 80;
         if(p != -1) {
             port = Integer.parseInt(host.substring(p + 1, host.length()));
             host = host.substring(0, p);
         }
-        String path = "/";
-        if(tokenizer.hasMoreTokens()) path += tokenizer.nextToken();
-        //ipAddress = InetAddress.getByName(host);
-        System.out.println("host: "+host);
-        System.out.println("port: "+port);
-        System.out.println("path: "+path);
-        System.out.println("request: "+getHttpRequest(path));
+        String path = getPath(tokenizer);
+        String request = getHttpRequest(path, host, port);
+        try{
+            InetAddress ipAddress = InetAddress.getByName(host);
+            Socket client = new Socket(ipAddress, port);
+            client.setReuseAddress(true);
+            client.setKeepAlive(true);
+            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+            dos.write(request.getBytes());
+            dos.flush();
+            DataInputStream dis = new DataInputStream(client.getInputStream());
+            byte[] responseBuffer = new byte[1024], buffer;
+            int read = dis.read(responseBuffer);
+            String responseHeader = new String(responseBuffer);
+            int offset = responseHeader.indexOf("\r\n\r\n") + 2;
+            responseHeader = responseHeader.substring(0, offset);
+            String lengthHeaderField = responseHeader.substring(
+                    responseHeader.indexOf("Content-Length: ") + 16);
+            lengthHeaderField = lengthHeaderField.substring(0,
+                    lengthHeaderField.indexOf("\r\n"));
+            int size = Integer.parseInt(lengthHeaderField.replace(" ", ""));
+            String contentType = responseHeader.substring(
+                    responseHeader.indexOf("Content-Type: ") + 14);
+            contentType = contentType.substring(0, contentType.indexOf("\r\n"));
+            ByteArrayInputStream bais = new ByteArrayInputStream(responseBuffer, offset, read);
+            buffer = new byte[read - offset + 1];
+            bais.read(buffer);
+            
+        } catch(Exception ex){
+            System.err.println("Fatal error: "+ex.getMessage());
+            ex.printStackTrace();
+            System.exit(1);
+        }
     }
     
-    private static String getHttpRequest(String rsc){
+    private static String getPath(StringTokenizer tokenizer){
+        StringBuilder pathBuilder = new StringBuilder();
+        pathBuilder.append("/");
+        while(tokenizer.hasMoreTokens()){
+            pathBuilder.append(tokenizer.nextToken());
+            pathBuilder.append("/");
+        }
+        return pathBuilder.toString();
+    }
+    
+    private static String getHttpRequest(String rsc, String host, int port){
         StringBuilder sb = new StringBuilder();
         sb.append("GET ");
         sb.append(rsc);
-        sb.append(" HTTP/1.0\n");
+        sb.append(" HTTP/1.0\r\n");
+        sb.append("Host: ");
+        sb.append(host);
+        if(port != 80){
+            sb.append(":");
+            sb.append(port);
+        }
+        sb.append("\r\nAccept: */*\r\nAccept-Encoding: gzip, deflate, br\r\n");
+        sb.append("Connection: keep-alive\r\n\r\n");
         return sb.toString();
     }
 }

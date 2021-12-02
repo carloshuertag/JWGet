10310258fc6a06faafd1c94d9a9f07ca7816a984
Carlos Huerta Garcia <huerta2502@gmail.com>
2021-12-02 15:07:53 +0000

    refactored main class

--- src/JWGet/JWGet.java
+++ src/JWGet/JWGet.java
@@ -1,11 +1,6 @@
 package JWGet;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -34,56 +29,11 @@ public class JWGet {
             host = host.substring(0, p);
         }
         String path = getPath(tokenizer);
-        String request = Utilities.getHttpRequest(path, host, port);
-        try{
-            InetAddress ipAddress = InetAddress.getByName(host);
-            Socket client = new Socket(ipAddress, port);
-            client.setReuseAddress(true);
-            client.setKeepAlive(true);
-            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-            dos.write(request.getBytes());
-            dos.flush();
-            DataInputStream dis = new DataInputStream(client.getInputStream());
-            byte[] responseBuffer = new byte[1024], buffer;
-            int read = dis.read(responseBuffer);
-            String responseHeader = new String(responseBuffer);
-            int offset = responseHeader.indexOf("\r\n\r\n") + 2;
-            responseHeader = responseHeader.substring(0, offset);
-            String lengthHeaderField = responseHeader.substring(
-                    responseHeader.indexOf("Content-Length: ") + 16);
-            lengthHeaderField = lengthHeaderField.substring(0,
-                    lengthHeaderField.indexOf("\r\n"));
-            int size = Integer.parseInt(lengthHeaderField.replace(" ", ""));
-            String contentType = responseHeader.substring(
-                    responseHeader.indexOf("Content-Type: ") + 14);
-            contentType = contentType.substring(0, contentType.indexOf("\r\n"));
-            initWorkingDir(); // if it is the first time that the program is executed
-            File file = new File(Utilities.DOWNLOADSPATH + Utilities.getParentPath(path));
-            file.mkdirs();
-            file.setWritable(true);
-            System.out.println(request);
-            System.out.println(responseHeader);
-            DataOutputStream fileDos = new DataOutputStream(
-                    new FileOutputStream(Utilities.DOWNLOADSDIR + path));
-            int received = read - offset + 1;
-            fileDos.write(responseBuffer, offset + 1, received);
-            while(received < size){
-                buffer = new byte[1024];
-                read = dis.read(buffer);
-                fileDos.write(buffer, 0, read);
-                fileDos.flush();
-                received += read;
-            }
-            fileDos.close();//file dos
-            dis.close();//socket dis
-            dos.close();//socket dos
-            if((recursive == 's' || recursive == 'S') && contentType.contains("htm")){
-                //html file stored analysis
-            }
-        } catch(Exception ex){
-            System.err.println("Fatal error: "+ex.getMessage());
-            ex.printStackTrace();
-            System.exit(1);
+        String contentType = Utilities.getResource(host, port, path);
+        if((recursive == 's' || recursive == 'S') && contentType.contains("htm")){
+            // html file stored in <Utilities.DOWNLOADSDIR + path> analysis
+            // seek for src= and href= html tags attributes
+            // if possible in a function to call rescursively (DFS)
         }
     }
     
